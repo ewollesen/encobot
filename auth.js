24a6b5cb2703f19d17d0826758dcdcc46e5d9b4b
Eric Wollesen <ericw@xmtp.net>
2012-01-12 08:36:26 -0700

    Remove turntable-api cruft

--- auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  auth: "auth+live+158120c84a085cf8ad5ba60a94ebea120c9ce68a",
-  userid: "4ee8354ca3f7517e38000f8a",
-  //roomid: "4ed7bc104fe7d01c8000061a" // lead media hipsters
-  //roomid: "4ee83377a3f7517e38000f7a" // encydo
-  //roomid: "4ef0bded590ca2457c000870" // The Nerd Pad
-  roomid: "4f0d92e8a3f7517a21000558" // Industrial 101
-};
--- examples/autobop.js
+++
@@ -1,9 +0,0 @@
-/**
- * Each time a song start, the bot vote up.
- */
-
-var Bot    = require('../index');
-
-var bot = new Bot(AUTH, USERID, ROOMID);
-
-bot.on('newsong', function (data) { bot.vote('up'); });
